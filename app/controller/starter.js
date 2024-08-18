1fb31d5cd051280181f738bbeeae44ab301fe840
wanthanai kaiphet <wanthanai.bass@nilecon.com>
2024-08-18 14:28:55 +0700

    update endpoint method post to get

--- app/controller/starter.js
+++ app/controller/starter.js
@@ -1,8 +1,6 @@
 const starter = require('../model/starter');
 
 exports.testController = async function(req, res) {
-    console.log(req.body);
-
     starter.testModel()   
     .then(response => {
         res.send(response);
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,5 @@ module.exports = function(app)
     var starter = require('../controller/starter');
 
     // app.route("/test").get(starter.testController);
-    app.route("/v2/test").post(starter.testController);
+    app.route("/v2/test").get(starter.testController);
 }
